fc8783fbc5e57e63e18a3ffd53e1e7c22b2cf223
Ben Cimperman <cimpe005@d.umn.edu>
2022-04-19 12:56:48 -0500

    hbj

--- part1/http.c
+++ part1/http.c
@@ -34,3 +34,67 @@ int write_http_response(int fd, const char *resource_path) {
     // TODO Not yet implemented
     return 0;
 }
+
+int read_http_response(int sock_fd, const char *file) {
+    // stdio FILE * gives us 'fgets()' to easily read line by line
+    int sock_fd_copy = dup(sock_fd);
+    if (sock_fd_copy == -1) {
+        perror("dup");
+        return -1;
+    }
+    FILE *socket_stream = fdopen(sock_fd_copy, "r");
+    if (socket_stream == NULL) {
+        perror("fdopen");
+        close(sock_fd_copy);
+        return -1;
+    }
+    // Disable the usual stdio buffering
+    if (setvbuf(socket_stream, NULL, _IONBF, 0) != 0) {
+        perror("setvbuf");
+        fclose(socket_stream);
+        return -1;
+    }
+
+    // Keep consuming lines until we find an empty line
+    // This marks the end of the response headers and beginning of actual content
+    char buf[BUFSIZE];
+    while (fgets(buf, BUFSIZE, socket_stream) != NULL) {
+        if (strcmp(buf, "\r\n") == 0) {
+            break;
+        }
+    }
+
+    if (fclose(socket_stream) != 0) {
+        perror("fclose");
+        return -1;
+    }
+
+    // Open destination file for writing
+    int file_fd = open(file, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+    if (file_fd == -1) {
+        perror("open");
+        return -1;
+    }
+
+    int bytes_read;
+    while ((bytes_read = read(sock_fd, buf, BUFSIZE)) > 0) {
+        if (write(file_fd, buf, bytes_read) == -1) {
+            perror("write");
+            close(file_fd);
+            return -1;
+        }
+    }
+    if (bytes_read == -1) {
+        perror("read");
+        close(file_fd);
+        return -1;
+    }
+
+    if (close(file_fd) != 0) {
+        perror("close");
+        return -1;
+    }
+
+    return 0;
+}
+
--- part1/http_server.c
+++ part1/http_server.c
@@ -19,6 +19,32 @@ void handle_sigint(int signo) {
     keep_going = 0;
 }
 
+// Makes a TCP server
+// host: Name of server to connect to
+// port: Port to connect to
+// Returns a new TCP socket file descriptor on success or -1 on error
+int open_server(const char *host, const char *port) {
+    // TODO Not yet implemented
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    struct addrinfo *server;
+    int ret_val = getaddrinfo(host, port, &hints, &server);
+    if(ret_val != 0){
+        printf("getaddrinfo failed: %s\n", gai_strerror(ret_val));
+    }
+    int sock_fd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+    if(sock_fd == -1){
+        perror("socket failure:\n");
+    }
+    connect(sock_fd, server->ai_addr, server->ai_addrlen);
+    return sock_fd;
+}
+
+
+
 int main(int argc, char **argv) {
     // First command is directory to serve, second command is port
     if (argc != 3) {
@@ -26,9 +52,24 @@ int main(int argc, char **argv) {
         return 1;
     }
     // Uncomment the lines below to use these definitions:
-    // const char *serve_dir = argv[1];
-    // const char *port = argv[2];
+    const char *serve_dir = argv[1];
+    const char *port = argv[2];
 
     // TODO Complete the rest of this function
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    struct addrinfo *server; 
+    int ret_val = getaddrinfo(host, port, &hints, &server);
+    if (ret_val !=0){
+        printf("getaddrinfo failure %s\n", gai_strerror(ret_val));
+    }
+    int sock_fd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+    if (sock_fd == -1){
+        perror("socket failure: \n");
+    }
+    bind(sock_fd, )
+    
     return 0;
 }
